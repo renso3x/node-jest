187006fbc20b5a3b237c3266f678aae18270ee9a
Romeo Enso <romeo.enso@ivegroup.com.au>
2022-08-16 08:35:52 +1000

    Setup NodeJS jest

---
+++ src/app/Utils.ts
@@ -0,0 +1,14 @@
+import { UrlWithParsedQuery, parse } from "url"
+
+export class Utils {
+
+  public static parseUrl(url: string): UrlWithParsedQuery {
+    if (!url) {
+      throw new Error('Empty Url')
+    }
+    return parse(url, true)
+  }
+  public static toUpperCase(text: string) {
+    return text.toUpperCase()
+  }
+}
